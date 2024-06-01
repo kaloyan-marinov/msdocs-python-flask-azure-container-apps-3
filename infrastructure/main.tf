27a5cb2be205a3edffe20059640171a06f4f4a56
Kaloyan Marinov <happy-2024-01-02-16-53@protonmail.com>
2024-06-01 13:03:46 +0200

    02: transition from Azure Container Apps to Azure App Service

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.52.0"
+      version = "=3.106.1"
     }
   }
 }
@@ -23,59 +23,53 @@ resource "azurerm_resource_group" "rg_m_p_f_a_c_a_3" {
   location = "West Europe"
 }
 
-resource "azurerm_log_analytics_workspace" "l_a_w" {
-  name                = "l-a-w"
-  location            = azurerm_resource_group.rg_m_p_f_a_c_a_3.location
+resource "azurerm_service_plan" "s_p" {
+  name                = "s-p"
   resource_group_name = azurerm_resource_group.rg_m_p_f_a_c_a_3.name
-  sku                 = "PerGB2018"
-  retention_in_days   = 30
+  location            = azurerm_resource_group.rg_m_p_f_a_c_a_3.location
+  os_type             = "Linux"
+  # sku_name            = "P1v2"
+  sku_name            = "F1"
 }
 
-resource "azurerm_container_app_environment" "c_a_e" {
-  name                       = "c-a-e"
-  location                   = azurerm_resource_group.rg_m_p_f_a_c_a_3.location
-  resource_group_name        = azurerm_resource_group.rg_m_p_f_a_c_a_3.name
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.l_a_w.id
-}
+resource "azurerm_linux_web_app" "l_w_a" {
+  name                = "l-w-a"
+  resource_group_name = azurerm_resource_group.rg_m_p_f_a_c_a_3.name
+  location            = azurerm_resource_group.rg_m_p_f_a_c_a_3.location
+  service_plan_id     = azurerm_service_plan.s_p.id
 
-# ( Based on https://medium.com/@vivazmo/azure-container-apps-with-terraform-part-1-ae20649e0dff : )
-resource "azurerm_container_app" "c_a" {
-  name                         = "c-a"
+  site_config {
+    # ( Based on https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/linux_web_app#site_config : )
+    # `always_on` must be explicitly set to false when using `Free`, `F1`, `D1`, or `Shared` Service Plans.
+    always_on = false
 
-  container_app_environment_id = azurerm_container_app_environment.c_a_e.id
-  resource_group_name          = azurerm_resource_group.rg_m_p_f_a_c_a_3.name
-  revision_mode                = "Single"
-
-  registry {
-    server               = "docker.io"    
-    
-    username = var.docker_io_username
-    password_secret_name = "docker-io-access-token"
-  }
-
-  ingress {
-    allow_insecure_connections = false
-    external_enabled           = true
-    target_port                = 5000
-    traffic_weight {
-      percentage = 100
-      # The following is added based on the recommendation at
-      # https://github.com/hashicorp/terraform-provider-azurerm/issues/20435#issuecomment-1443418097 .
-      latest_revision = true
+    application_stack {
+      docker_image_name        = "${var.docker_io_username}/${var.name_of_container_image}:${var.tag_for_container_image}"
+      docker_registry_url      = "https://docker.io"
+      # docker_registry_username = var.docker_io_username
+      # docker_registry_password = var.docker_io_access_token
     }
   }
 
-  template {
-    container {
-      name   = "template-container-m-p-f-a-c-a-3"
-      image  = "${var.docker_io_username}/${var.name_of_container_image}:${var.tag_for_container_image}"
-      cpu    = 0.25
-      memory = "0.5Gi"
-    }
-  }
-
-  secret {
-    name = "docker-io-access-token"
-    value = var.docker_io_access_token
+  # Despite what the following resources state/claim,
+  # commenting out the next block
+  # does _not_ hinder the Azure App Service
+  # from understanding/guessing what port the (above-specified) container is listening on!
+  #
+  # https://duckduckgo.com/?q=how+to+tell+azurerm_linux_web_app+what+port+the+deployed+container+is+listening+on&atb=v413-1&ia=web
+  #
+  #   https://learn.microsoft.com/en-us/azure/app-service/reference-app-settings?tabs=kudu%2Cdotnet
+  #     `WEBSITES_PORT`
+  #     For a custom container, the custom port number on the container for App Service to route requests to.
+  #     By default, App Service attempts automatic port detection of ports 80 and 8080.
+  #     This setting isn't injected into the container as an environment variable.
+  #
+  #   https://stackoverflow.com/questions/72705694/specify-port-while-deploying-a-docker-container-for-terraform-provider-for-azure
+  #
+  #     https://github.com/hashicorp/terraform-provider-azurerm/blob/main/examples/app-service/docker-basic/main.tf
+  app_settings = {
+    # For details, you can consult
+    # https://learn.microsoft.com/en-us/azure/app-service/reference-app-settings?tabs=kudu%2Cdotnet#custom-containers
+    "WEBSITES_PORT" = "5000"
   }
 }
--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,11 +1,3 @@
-output "azurerm_container_app_url" {
-  # value = azurerm_container_app.c_a.latest_revision_fqdn
-  # (
-  # Based on
-  # https://stackoverflow.com/a/75803638
-  # and
-  # https://github.com/hashicorp/terraform-provider-azurerm/issues/20696#issuecomment-1578567585 ,
-  # the following is an improvement upon the former :
-  # )
-  value = "https://${azurerm_container_app.c_a.ingress[0].fqdn}"
+output "azurerm_linux_web_app_url" {
+  value = "https://${azurerm_linux_web_app.l_w_a.default_hostname}"
 }
